27ec4675ebb9b177158741687454586303d23ab3
Ana Mendoza <mendozanart@gmail.com>
2022-02-20 17:41:19 -0300

    conexion itineraries

---
+++ backend/models/itineraries.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const itinerariesSchema = new mongoose.Schema ({
+
+    city:{type:String,require:true},    
+    title:{type:String,require:true},
+    description:{type:String,require:true},
+    schedule:{type:Number,require:true},
+    price:{type:Number,require:true},
+    img: [{
+        type:String,require:true,
+    }]
+})
+
+const Itinerary = mongoose.model("Itineraries",itinerariesSchema)
+
+module.exports = Itinerary;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,8 +1,13 @@
 const Router = require("express").Router();
 const citiesController = require("../controlers/datosControllers")
+const itinerariesController = require ("../controlers/itinerariosControllers.js")
 const {ObtenerDatos} = citiesController //destructuracion del controlador
+const {ObtenerItinerarios} = itinerariesController
 
 Router.route("/datos") //url de consulta
 .get(ObtenerDatos)
+
+Router.route("/itinerarios") //url de consulta
+.get(ObtenerItinerarios)
 
 module.exports = Router
